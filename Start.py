6937cf8a9dce99f18feb56db4ae2ed2db3afe04a
TeryVeneno <40150870+TeryVeneno@users.noreply.github.com>
2019-09-10 14:03:22 -0500

    1

--- Start.py
+++ Start.py
@@ -1,1 +1,18 @@
+my_int = 2
+my_float = 3
+my_string = "Hello."
+g, f = 6, 7
 
+print("float: ", my_float)
+print("integer: ", my_int)
+print("string: ", my_string)
+print("Multi-Assignment: ", g, f)
+
+my_tens = [10, 20, 30]
+my_fives = [5, 15, 25]
+my_tens.append(40)
+my_fives.append(35)
+
+print("Cubed", my_int ** my_float)
+print("Repeating Sequence: "+my_string*3)
+print("Combining lists: ", my_tens+my_fives)
---
+++ first_script.py
@@ -0,0 +1,10 @@
+mystring = "hello"
+myint = 20
+myfloat = 10.0
+
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
